660273b76d223324fc6cbe3bf3235d69d346ffed
James McDonald <jmcscript@gmail.com>
2025-07-16 10:14:28 -0500

    state corresponds to 03_01

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
 
+/**
+ * @typedef {article[]} articleInfo
+ *
+ */
+
+/**
+ * @typedef {Object} article
+ * @property {string} name
+ * @property {number} upvotes
+ * @property {Object[]} comments
+ */
+
+/** @type {articleInfo} */
 const articleInfo = [
-  { name: 'learn-node', upvotes: 0 },
-  { name: 'learn-react', upvotes: 0 },
-  { name: 'learn-mongodb', upvotes: 0 },
+  { name: 'learn-node', upvotes: 0, comments: [] },
+  { name: 'learn-react', upvotes: 0, comments: [] },
+  { name: 'learn-mongodb', upvotes: 0, comments: [] },
 ];
 
 /**
@@ -19,33 +32,31 @@ app.use(express.json());
  * Endpoints
  */
 
-// app.get('/hello', function (req, res) {
-//   res.send('Hello from a GET endpoint!');
-// });
-
-// app.get('/hello/:name', function (req, res) {
-//   const name = req.params.name;
-//   res.send(`Hello ${name}, from a GET endpoint!`);
-// });
-
-// app.post('/hello', function (req, res) {
-//   const name = 'name' in req.body ? req.body.name : null;
-
-//   if (name) {
-//     res.send(`Hello ${name}, from a POST endpoint!`);
-//   }
-
-//   res.send('Hello from a POST endpoint!');
-// });
-
 app.post('/api/articles/:name/upvote', (req, res) => {
   const article = articleInfo.find((a) => a.name === req.params.name) || null;
 
   if (article) {
     ++article.upvotes;
-    res.send(`Success, the ${req.params.name} article now has ${article.upvotes} upvotes.`);
+    res.json(article);
   } else {
     res.send(`Failure, the ${req.params.name} article could not be found.`);
+  }
+});
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articleInfo.find((a) => a.name === name) || null;
+
+  if (article && 'comments' in article) {
+    article.comments.push({
+      postedBy,
+      text,
+    });
+    res.json(article);
+  } else {
+    res.send(`Failure, the ${name} article could not be found.`);
   }
 });
 
